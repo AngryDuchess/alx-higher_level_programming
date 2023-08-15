f93bcd20ee7a25c9bdce9718207daf6c55b39420
Mahama Hamida <mahamahamida@gmail.com>
2023-08-15 11:42:27 +0000

    infinite add"

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arg_length = len(sys.argv) - 1
+    arguments = sys.argv[1:]
+
+    if arg_length == 0:
+        print("0")
+    else:
+        count = 0
+        for arg in arguments:
+          count += int (arg)
+
+        print(count)
