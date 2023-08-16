06bec751f9eff230c0e2195051e5f8812bf1669a
Mahama Hamida <mahamahamida@gmail.com>
2023-08-16 17:16:10 +0000

    list

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,4 +1,4 @@
-#!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
+#!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
