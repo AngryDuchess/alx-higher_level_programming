59827b1e382eca5767391654da11f603330613b2
Mahama Hamida <mahamahamida@gmail.com>
2023-08-18 14:38:09 +0000

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence == "":
+        return None, None
+
     tuple_length = len(sentence)
     first = sentence[0]
     return (tuple_length, first)
