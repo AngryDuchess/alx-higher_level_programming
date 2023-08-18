5e180f88ebf5249f35fa9a7c01a721f3300f03b2
Mahama Hamida <mahamahamida@gmail.com>
2023-08-18 13:46:13 +0000

    returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
+def multiple_returns(sentence):
+    tuple_length = len(sentence)
+    first = sentence[0]
+    return (tuple_length, first)
