fb1190989e7d51c8d2a92a187068aace932630f8
Mahama Hamida <mahamahamida@gmail.com>
2023-08-29 00:39:39 +0000

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             i += 1
+        print()
+        return i
     except IndexError:
         pass
-    print()
-    return i
+        print()
+        return i
