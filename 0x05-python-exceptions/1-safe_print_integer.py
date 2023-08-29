30976780ebacea1c6d0dca0cce71680596fa7dd5
Mahama Hamida <mahamahamida@gmail.com>
2023-08-29 01:17:58 +0000

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     int_val = str(value)
     try:
-        if (int_val.isdigit() or (int_val[0] == '-' and int_val[1:].isdigit())):
+        if (int_val.isdigit() or (int_val[0] == '-' and
+           int_val[1:].isdigit())):
             print("{:d}".format(int(int_val)))
             return True
     except ValueError:
