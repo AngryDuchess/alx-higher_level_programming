4314fd846af43f38d003c70e34d640cd772c4c6c
Mahama Hamida <mahamahamida@gmail.com>
2023-08-30 01:13:48 +0000

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,6 +12,7 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be >= 0")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
