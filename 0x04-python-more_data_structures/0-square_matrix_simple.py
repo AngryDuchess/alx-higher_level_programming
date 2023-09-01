abdeb3f19c6d312fa772da06b462962b7ca705b3
Mahama Hamida <mahamahamida@gmail.com>
2023-09-01 07:54:09 +0000

    square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""initializing script"""
+
+def square_matrix_simple(matrix=[]):
+    """this function squares each element in a matrix
+    Args:
+    matrix = matrix to be squared  
+    """
+
+    new_matrix = [[i**2 for i in row] for row in matrix]
+    """create new matrix using nested list comprehension"""
+    return new_matrix
