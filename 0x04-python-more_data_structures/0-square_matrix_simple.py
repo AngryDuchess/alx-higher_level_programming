5ea0cf401e810acad4bf676f7906f8f8d48e46bc
Mahama Hamida <mahamahamida@gmail.com>
2023-09-01 07:57:10 +0000

    trailing whitespace

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """initializing script"""
 
+
 def square_matrix_simple(matrix=[]):
     """this function squares each element in a matrix
     Args:
-    matrix = matrix to be squared  
+    matrix = matrix to be squared
     """
 
     new_matrix = [[i**2 for i in row] for row in matrix]
