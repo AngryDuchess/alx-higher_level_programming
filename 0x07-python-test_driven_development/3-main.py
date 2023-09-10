18a55b81ef1c82fea296566b05e1192047fe3b75
Mahama Hamida <mahamahamida@gmail.com>
2023-09-10 13:29:40 +0000

    fix

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,7 +1,7 @@
 #!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name(None)
+say_my_name()
 say_my_name("24", "White")
 say_my_name("Bob")
 try:
