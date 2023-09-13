ca06d897c29e16b34659a17f2cd0a00668037989
Mahama Hamida <mahamahamida@gmail.com>
2023-09-13 02:18:47 +0000

    last test

---
+++ 0x07-python-test_driven_development/6-main.py
@@ -0,0 +1,5 @@
+#!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([444444444]))
+print(max_integer([1, 3, 4, 2]))
