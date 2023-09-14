4d5e8e826798f7b3f69eafe77545677bf5849b74
Mahama Hamida <mahamahamida@gmail.com>
2023-09-14 02:10:41 +0000

    more classes

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,7 @@
+#!/c/Users/maham/AppData/Local/Microsoft/WindowsApps/python
+Rectangle = __import__('0-rectangle').Rectangle
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
+
