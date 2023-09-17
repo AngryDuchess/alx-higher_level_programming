a7745e965a7563cbc7f32af8089d1d7cd1c0d967
Mahama Hamida <mahamahamida@gmail.com>
2023-09-17 22:11:45 +0000

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module contains a class MyInt
+
+"""
+
+class MyInt(int):
+    """class that inherits from int"""
+    def __eq__(self, num):
+        """function for equivalence"""
+        return(int(self) != int(num))
+
+    def __ne__(self, num):
+        """function for not equal"""
+        return (int(self) == int (num))
