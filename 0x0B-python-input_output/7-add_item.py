b4a36bc0c60ca549953c4a430b76823ecf1946d6
Mahama Hamida <mahamahamida@gmail.com>
2023-09-18 23:40:13 +0000

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,17 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
-    """populate list"""
-    my_list = []
+    """check if file exists"""
+    filename = "add_item.json"
+
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
     arguments = sys.argv[1:]
     for argument in arguments:
-        my_list.append(argument)
+        my_list.extend(argument)
 
     """save file"""
     filename = "add_item.json"
     save_to_json_file(my_list, filename)
-
-    """load the file"""
-    loaded_list = load_from_json_file(filename)
