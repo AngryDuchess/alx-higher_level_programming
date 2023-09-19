078cfede2bf534f47f4c7a4a9e8e1ba171b3ca26
Mahama Hamida <mahamahamida@gmail.com>
2023-09-19 03:38:51 +0000

    9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this module contains the class Student"""
+
+
+class Student:
+    """class for a student"""
+    
+    def __init__(self, first_name, last_name, age):
+        """initializes object and other arguments
+        Args:
+        first_name: first name
+        last_name: last name
+        age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
