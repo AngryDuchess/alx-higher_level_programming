df1b9a8a7da30247f18823beea08e17c4e522acd
Mahama Hamida <mahamahamida@gmail.com>
2023-09-19 05:17:45 +0000

    rect

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+    this module contains class Rectangle which inherits from the Base class
+
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """a getter to retriece the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """a setter to set the value of the width
+        Args:
+            value: value of width
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """a getter to retriece the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """a setter to set the value of the height
+        Args:
+            value: value of height
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """a getter to retrieve x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """a setter to set the value of x
+        Args:
+            value: value of x
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """a getter to retrieve y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """a setter to set the value of y
+        Args:
+            value: value of y
+        """
+        self.__y = value
