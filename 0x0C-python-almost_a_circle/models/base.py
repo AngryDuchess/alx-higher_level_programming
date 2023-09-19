f811fa8eb804fe4f3248ebdae4ad18174f61a078
Mahama Hamida <mahamahamida@gmail.com>
2023-09-19 04:39:21 +0000

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+"""this module contains the Base class"""
+
+
+class Base:
+    """the base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """initializing object and arguments"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
