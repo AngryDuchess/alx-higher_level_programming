4fb06e24eed707db716e53d9fedf125339dc8169
Mahama Hamida <mahamahamida@gmail.com>
2023-09-21 16:35:49 +0000

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the json representation"""
         if list_dictionaries is None:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
