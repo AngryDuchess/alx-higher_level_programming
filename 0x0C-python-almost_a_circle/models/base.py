b2e742f499be4a101029de7769acd4d40265e44f
Mahama Hamida <mahamahamida@gmail.com>
2023-09-21 21:37:56 +0000

    f

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,6 +43,6 @@ class Base:
     def from_json_string(json_string):
         """takes a json string and returns a list of
         json string representation"""
-        if json_string is None or len(json_string) == []:
+        if json_string is None or len(json_string) == 0:
             return "[]"
         return json.loads(json_string)
