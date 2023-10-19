5fcb5a663e1e02ba52378385afddad99fe7c3bfd
Mahama Hamida <mahamahamida@gmail.com>
2023-10-19 15:48:13 -0700

    fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON genre_id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
