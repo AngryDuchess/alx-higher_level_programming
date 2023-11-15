8b78fcfeece4392afa0de03ca4c5d51794ae7248
Mahama Hamida <mahamahamida@gmail.com>
2023-11-15 00:53:43 +0000

    7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+let num = parseInt(argv[2]);
+
+if (!num) {
+  console.log('Missing number of occurrences');
+} else {
+  while (num > 0) {
+    console.log('C is fun');
+    num--;
+  }
+}
