529da7c367d80383fdb9a4e31fe163e36369e3d4
Mahama Hamida <mahamahamida@gmail.com>
2023-11-15 01:32:27 +0000

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function fact(num) {
+  if (num === 0 || isNaN(num)) {
+    return 1;
+}
+return (fact(num - 1) * num);
+}
+
+const num = parseInt(argv[2]);
+console.log(fact(num));
