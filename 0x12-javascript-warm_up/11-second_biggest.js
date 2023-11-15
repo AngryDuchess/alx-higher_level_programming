a46652d15c35b557abf286601cab4847c8d96456
Mahama Hamida <mahamahamida@gmail.com>
2023-11-15 08:08:12 +0000

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
 const args = process.argv.slice(2);
-if (args.length <= 1){
+const toNumber = args.map(Number);
+if (args.length <= 1) {
   console.log('0');
 } else {
-  const toNumber = args.map(Number);
-  const sortedNum = toNumber.sort((a,b) => b - a);
+  /*const toNumber = args.map(Number);*/
+  const sortedNum = toNumber.sort((a, b) => b - a);
   console.log(sortedNum[1]);
 }
