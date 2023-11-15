6a1755d27acfcaaeb861f6b73b0c73ce96fb2949
Mahama Hamida <mahamahamida@gmail.com>
2023-11-15 08:21:57 +0000

    13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const toNumber = args.map(Number);
 if (args.length <= 1) {
   console.log('0');
 } else {
-  /*const toNumber = args.map(Number);*/
+  /* const toNumber = args.map(Number); */
   const sortedNum = toNumber.sort((a, b) => b - a);
   console.log(sortedNum[1]);
 }
