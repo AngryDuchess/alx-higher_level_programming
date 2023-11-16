9aa7befd1da42632df450ffa8dc076de4c74c2c6
Mahama Hamida <mahamahamida@gmail.com>
2023-11-16 00:01:28 +0000

    done with mandatory

--- 0x13-javascript_objects_scopes_closures/6-square2.js
+++ 0x13-javascript_objects_scopes_closures/6-square2.js
@@ -2,7 +2,10 @@
 const SquareMain = require('./5-square');
 
 class Square extends SquareMain {
-  
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
