a060ee10c9a1ad232de1d18aa7625b1f68f2664d
Mahama Hamida <mahamahamida@gmail.com>
2023-11-19 12:16:54 +0000

    task 9 done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,23 +2,23 @@
 """
 lists all state objects from a specified database
 """
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import State, Base
-from sys import argv
+if __name__ == "__main__":
+    from sqlalchemy.orm import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import State, Base
+    from sys import argv
 
+    MY_USER, MY_PASS, MY_DB = argv[1:]
 
-MY_USER, MY_PASS, MY_DB = argv[1:]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(MY_USER, MY_PASS, MY_DB),
+                           pool_pre_ping=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(MY_USER, MY_PASS, MY_DB),
-                       pool_pre_ping=True)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-states = session.query(State).order_by(State.id)
-for row in states:
-    print("{}: {}".format(row.id, row.name))
-session.close()
+    states = session.query(State).order_by(State.id)
+    for row in states:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
