e70df145d80b555dc8678ac8987a5f95f9418e37
Mahama Hamida <mahamahamida@gmail.com>
2023-11-19 14:44:45 +0000

    12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+updates an existing new object in the databse
+"""
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+    from sys import argv
+
+    MY_USER, MY_PASS, MY_DB = argv[1:]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(MY_USER, MY_PASS, MY_DB),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_obj = session.query(State).where(State.id == 2).first()
+    state_obj.name = 'New mexico'
+    session.commit()
+    session.close()
