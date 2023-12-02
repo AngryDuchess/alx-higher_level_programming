1d7b4670f247a36fce63212411924bf0e99cd2da
Mahama Hamida <mahamahamida@gmail.com>
2023-12-02 01:12:14 +0000

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
-    req = requests.post(url, email)
+    req = requests.post(url, data=email)
     print(req.text)
