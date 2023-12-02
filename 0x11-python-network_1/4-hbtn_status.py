0feb428b6fff1e88b77eeb83ae3d7f9dd5608e97
Mahama Hamida <mahamahamida@gmail.com>
2023-12-02 00:48:10 +0000

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+fetch url using request library
+"""
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    txt = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(txt)))
+    print("\t- content: {}".format(txt))
