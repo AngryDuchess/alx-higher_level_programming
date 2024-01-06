071426ed15a43bd6968cfe90f3f6040eec15aa6d
Mahama Hamida <mahamahamida@gmail.com>
2024-01-06 15:04:45 +0000

    fix: task 2 semi standard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const {error} = require('console')
-const request = require('request')
+const { error } = require('console');
+const request = require('request');
 
-const [, , URL] = process.argv
+const [, , URL] = process.argv;
 
 request(URL, (error, res) => {
-    console.log(`code: ${res.statusCode}`);
-})
+  console.log(`code: ${res.statusCode}`);
+});
