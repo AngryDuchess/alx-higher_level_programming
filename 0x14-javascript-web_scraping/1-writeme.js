77e58b4add60348d1eb01f897cc9483f07970555
Mahama Hamida <mahamahamida@gmail.com>
2024-01-06 14:09:45 +0000

    fix: semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-const [, , filePath, content] = process.argv
+const fs = require('fs');
+const [, , filePath, content] = process.argv;
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
-    console.log(err)
-})
+  console.log(err);
+});
